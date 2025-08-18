3c0e1d5d5f8efe17342a300c58ea71ad7cc8d77a
kino <kino@pop-os.localdomain>
2025-08-18 03:38:24 +0200

    (\|) ._. (|/)

--- solutions/partial_sums/src/lib.rs
+++ solutions/partial_sums/src/lib.rs
@@ -1,8 +1,7 @@
 pub fn parts_sums(arr: &[u64]) -> Vec<u64> {
-    let mut res = Vec::with_capacity(arr.len()+1);
-    let mut tot = arr.iter().sum();
+    let mut res = Vec::with_capacity(arr.len() + 1);
+    let mut tot: u64 = arr.iter().sum();
     res.push(tot);
-
     for n in arr.iter().rev() {
         tot -= n;
         res.push(tot);
