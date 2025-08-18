3e9b95b1642cce70de9ba39d4c6a2b476ac200d2
kino <kino@pop-os.localdomain>
2025-08-18 06:50:53 +0200

    (\|) ._. (|/)

--- solutions/order_books/src/lib.rs
+++ solutions/order_books/src/lib.rs
@@ -1,25 +1,23 @@
 pub use library::writers::Writer;
-pub mod library {
-    pub mod books {
-        #[derive(Debug, Clone)]
-        pub struct Book {
-            pub title: String,
-            pub year: u64,
-        }
-    }
 
+pub mod library {
     pub mod writers {
         use super::books::Book;
-
-        #[derive(Debug, Clone)]
+        #[derive(Debug, Eq, PartialEq)]
         pub struct Writer {
             pub first_name: String,
             pub last_name: String,
             pub books: Vec<Book>,
         }
     }
+    pub mod books {
+        #[derive(Debug, Eq, PartialEq)]
+        pub struct Book {
+            pub title: String,
+            pub year: u32,
+        }
+    }
 }
-
 pub fn order_books(writer: &mut Writer) {
-    writer.books.sort_by(|a,b| a.title.cmp(&b.title))
+    writer.books.sort_by(|a, b| a.title.cmp(&b.title));
 }
